200e2ac6b3253a4a0b7fa9c958624e4f5f2f2288
Kakelys <alexglobal1459@gmail.com>
2023-03-16 19:47:41 +0300

    init Nancy

--- API/app/Program.cs
+++ API/app/Program.cs
@@ -1,6 +1,8 @@
+using Nancy.Owin;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseOwin(x => x.UseNancy());
 
 app.Run();
